fca68af39c4d47fe87d94f6b04ef143396c6a49e
antonio-st <antonio-st@yandex.ru>
2022-10-28 23:16:23 +0300

    add freecodecamp lesson

---
+++ freecodecamp/less1/JS/index.js
@@ -0,0 +1,59 @@
+
+
+// Setup
+var a;
+
+// Only change code below this line
+
+
+// Setup
+var a;
+a = 7;
+var b;
+b = a;
+// Only change code below this line
+
+
+var myVar = 0;
+var a = 9;
+
+
+//Create two new string variables: myFirstName and myLastName and assign 
+// them the values of your first and last name, respectively.
+
+var myFirstName = 'Anton';
+var myLastName = 'Bogdanov';
+
+/*
+Initialize the three variables a, b, and c with 
+5, 10, and "I am a" respectively so that they will not be undefined.
+*/
+
+// Only change code below this line
+var a = 5;
+var b = 10;
+var c = "I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+
+
+
+
+/*
+Modify the existing declarations and assignments so their names use camelCase.
+Do not create any new variables.
+*/
+
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
