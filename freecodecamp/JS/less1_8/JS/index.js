8526ec5f19a14244a4b2bec11f81293dfa1c61a5
antonio-st <antonio-st@yandex.ru>
2022-10-29 18:36:44 +0300

    add new js lesson

---
+++ freecodecamp/JS/less1_8/JS/index.js
@@ -0,0 +1,12 @@
+var camper = "James";
+var camper = "David";
+console.log(camper);
+
+
+//let campers = "Max";
+let campers = "Antony";
+
+let catName = "Oliver";
+let catSound = "Meow!";
+
+console.log(campers, catName, catSound);
