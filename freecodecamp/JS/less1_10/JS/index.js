540c3b5decb6f34bfa4dd3c785bc9033fee0afe9
antonio-st <antonio-st@yandex.ru>
2022-10-30 08:59:13 +0300

    add lesson js 9-11

---
+++ freecodecamp/JS/less1_10/JS/index.js
@@ -0,0 +1,11 @@
+const sum = 10 + 10;
+console.log('10 + 10 =', sum)
+
+const difference = 45 - 33;
+console.log('45 - 33 =', difference)
+
+const product = 8 * 10;
+console.log('8 * 10 =', product)
+
+const quotient = 66 / 33;
+console.log('66 / 33 =', quotient)
---
+++ freecodecamp/JS/less1_11/JS/index.js
@@ -0,0 +1,26 @@
+let myVar = 87;
+// Only change code below this line
+myVar++; // myVar = myVar + 1
+console.log('let myVar = 87; \n myVar++; \n', myVar)
+
+
+
+let myVar2 = 11;
+// Only change code below this line
+myVar2--; // myVar2 = myVar2 - 1
+console.log('let myVar2 = 11; \n myVar--; \n', myVar2)
+
+
+const decimal = 5.7;
+// Only change code below this line
+let myDecimal = 10.1;
+console.log(decimal, myDecimal)
+
+
+
+const product = 2.0 * 2.5;
+console.log('2.0 * 2.5 =', product)
+
+
+const quotient = 4.4 / 2.0;
+console.log(quotient)
