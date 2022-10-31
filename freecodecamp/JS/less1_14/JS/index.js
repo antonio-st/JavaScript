b22e409cf5364abd4b42a72c4fd9458d2562b55b
antonio-st <antonio-st@yandex.ru>
2022-10-31 19:12:49 +0300

    add lesson on js

---
+++ freecodecamp/JS/less1_14/JS/index.js
@@ -0,0 +1,18 @@
+const myStr = "This is the start. " + 'This is the end.'; // Change this line
+console.log(myStr)
+
+let myStr2 = 'This is the first sentence. ';
+myStr2 += 'This is the second sentence.'
+console.log(myStr2)
+
+// Only change code below this line
+const myName = "Anton";
+const myStr3 = "My name is " + myName + ' and I am well!';
+console.log(myStr3)
+
+
+// Change code below this line
+const someAdjective = "Java Script";
+let myStr4 = "Learning to code is ";
+myStr4 += someAdjective;
+console.log(myStr4)
---
+++ freecodecamp/JS/less1_15/JS/index.js
@@ -0,0 +1,25 @@
+// Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// Only change code below this line
+lastNameLength = lastName.length;
+
+console.log('lastNameLength Length = ', lastNameLength)
+
+// Setup
+let firstLetterOfLastName = "";
+const lastName2 = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName2[0]; // Change this line
+
+console.log('lastName2[0] = ', firstLetterOfLastName)
+
+// Setup
+let myStr = "Jello World";
+
+// Only change code below this line
+myStr = "Hello World"; // Change this line
+// Only change code above this line
+console.log(myStr)
