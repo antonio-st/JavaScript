a837d96fa6173b5440d8b8817049d9debddefa1a
antonio-st <antonio-st@yandex.ru>
2022-10-31 19:36:59 +0300

    add lesson on js 15-16

--- freecodecamp/JS/less1_15/JS/index.js
+++ freecodecamp/JS/less1_15/JS/index.js
@@ -23,3 +23,37 @@ let myStr = "Jello World";
 myStr = "Hello World"; // Change this line
 // Only change code above this line
 console.log(myStr)
+
+
+// Setup
+const lastName3 = "Lovelace";
+
+// Only change code below this line
+const thirdLetterOfLastName = lastName3[2]; // Change this line
+console.log('lastName3[2] = ', thirdLetterOfLastName)
+
+
+// Setup
+const lastName4 = "Lovelace";
+// Only change code below this line
+const lastLetterOfLastName = lastName4[lastName4.length - 1]; // Change this line
+console.log('lastName4[lastName4.length - 1] = ', lastLetterOfLastName)
+
+
+// Setup
+const lastName5 = "Lovelace";
+// Only change code below this line
+const secondToLastLetterOfLastName = lastName5[lastName5.length - 2]; // Change this line
+console.log('lastName5[lastName5.length - 2] = ', secondToLastLetterOfLastName)
+
+//Word Blanks
+
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = myAdjective + " " + myNoun + " " + myAdverb + " " + myVerb; // Change this line
+// Only change code above this line
+console.log(wordBlanks)
---
+++ freecodecamp/JS/less1_16/JS/index.js
@@ -0,0 +1,5 @@
+//Store Multiple Values in one Variable using JavaScript Arrays
+
+// Only change code below this line
+const myArray = ['Java Script', 28, 'Python'];
+console.log(myArray)
