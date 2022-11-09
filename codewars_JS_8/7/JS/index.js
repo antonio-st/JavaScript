22770e52d79656d755c16ce3fd33fa1a684459b9
antonio-st <antonio-st@yandex.ru>
2022-11-09 22:31:15 +0300

    add 7 task codewars

---
+++ codewars_JS_8/7/JS/index.js
@@ -0,0 +1,6 @@
+function getAge(inputString) {
+    // return the girl's correct age as an integer. Happy coding :)
+    return num = parseInt(inputString[0], 10);
+}
+
+console.log(getAge('1years'))
