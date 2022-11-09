3c6f647017bfe9f19f6724370024f3f51e512352
antonio-st <antonio-st@yandex.ru>
2022-11-09 23:14:29 +0300

    add lesson 16-17 CSS basic

--- codewars_JS_8/7/JS/index.js
+++ codewars_JS_8/7/JS/index.js
@@ -1,6 +1,8 @@
+let youYear = prompt('Enter you age from [0...9]: ')
+
 function getAge(inputString) {
     // return the girl's correct age as an integer. Happy coding :)
     return num = parseInt(inputString[0], 10);
 }
 
-console.log(getAge('1years'))
+console.log(getAge(youYear))
