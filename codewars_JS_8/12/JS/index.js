1e55a742d2c5b71e3ae1c9d15f4abb593c060faf
antonio-st <antonio-st@yandex.ru>
2022-11-16 23:04:54 +0300

    add lesson 12 codewars

---
+++ codewars_JS_8/12/JS/index.js
@@ -0,0 +1,10 @@
+function howManyLightsabersDoYouOwn(name) {
+    if (name == 'Zach') {
+        return 18;
+    }
+    else {
+        return 0;
+    }
+}
+
+console.log(howManyLightsabersDoYouOwn('Zach'))
