6dc7c231c79637969cf551af1adeb33e8bccf7f9
antonio-st <antonio-st@yandex.ru>
2022-11-27 21:30:29 +0300

    add css lesson 18  and js lesson 39-40

---
+++ freecodecamp/JS/less1_40/JS/index.js
@@ -0,0 +1,7 @@
+function isLess(a, b) {
+    // Only change code below this line
+    return a < b;
+    // Only change code above this line
+  }
+  
+  console.log(1022, 1105, '=>', isLess(1022, 1105));
