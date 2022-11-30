4c28c1f002c911f81a0f66e8ddbe6be7818ec636
antonio-st <antonio-st@yandex.ru>
2022-11-30 21:53:14 +0300

    add task 43-44 JS

---
+++ freecodecamp/JS/less1_43_objects/JS/index.js
@@ -0,0 +1,10 @@
+const myDog = {
+    // Only change code below this line
+  'name': 'Barsik',
+  legs: 4, 
+  tails: 1, 
+  'friends' : ['Bobik', 'Barbos', 'Layka']
+    // Only change code above this line
+  };
+
+console.log(myDog.friends);
