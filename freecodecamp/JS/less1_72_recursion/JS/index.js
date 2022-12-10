0704d2b8aff0da58975723640250282dd553f957
antonio-st <antonio-st@yandex.ru>
2022-12-10 17:35:32 +0300

    added tasks 71-72 on JS

---
+++ freecodecamp/JS/less1_72_recursion/JS/index.js
@@ -0,0 +1,17 @@
+// function countup(n) {
+//     if (n < 1) {
+//       return [];
+//     } else {
+//       const countArray = countup(n - 1);
+//       countArray.push(n);
+//       return countArray;
+//     }
+//   }
+//   console.log(countup(5));
+  
+
+function countdown(n){
+    return n < 1 ? [] : [n].concat(countdown(n - 1));
+ }
+ 
+ console.log(countdown(5));
