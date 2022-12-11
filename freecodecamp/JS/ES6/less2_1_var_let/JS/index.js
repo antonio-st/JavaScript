069c604ee1ddc4be83842ca8a7750d45444b5ab7
antonio-st <antonio-st@yandex.ru>
2022-12-11 09:27:06 +0300

    added JS Basic folder, new folder ES6 on JS

---
+++ freecodecamp/JS/ES6/less2_1_var_let/JS/index.js
@@ -0,0 +1,24 @@
+// function checkScope() {
+//     var i = 'function scope';
+//     if (true) {
+//       var i = 'block scope';
+//       console.log('Block scope i is: ', i);
+//     }
+//     console.log('Function scope i is: ', i);
+//     return i;
+//   }
+
+//   console.log(checkScope());
+
+
+  function checkScope() {
+    let i = 'function scope';
+    if (true) {
+      let i = 'block scope';
+      console.log('Block scope i is: ', i);
+    }
+    console.log('Function scope i is: ', i);
+    return i;
+  }
+
+  console.log(checkScope());
